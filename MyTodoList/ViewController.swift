7a73a7ed81705a74364c597d5a1d67970b45f9af
rskrsk777 <ryoosk777@outlook.jp>
2018-12-31 17:43:27 +0900

    Initial Commit

---
+++ MyTodoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyTodoList
+//
+//  Created by ryosuke kubo on 2018/12/31.
+//  Copyright © 2018 ryosuke kubo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
